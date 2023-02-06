c836cb198ce13b0bc2cc0615c86b8978dc4af0bb
SSong-SSong <gksthf3609@gmail.com>
2023-02-06 13:59:05 +0900

    웹페이지

--- shs_test.py
+++ shs_test.py
@@ -1,14 +1,21 @@
 import streamlit as st
 
-import streamlit as st
+# 탭 적용 : PJ1, PJ2, PJ3
+tab1, tab2, tab3 = st.tabs(['PJ1', 'PJ2', 'PJ3'])
 
-with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
+with tab1:
+    st.header("Green Remodeling")
+    st.image("그린리모델링.jpg", width=500)
+    st.text("2016년 이후로 그린리모델링 업무를 수행해왔습니다.")
 
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
+
+with tab2:
+    st.header("Regression Metro")
+    st.text("이것은 철도기술연구원3차원 공간정보 기반 철도역사 용역입니다.")
+
+with tab3:
+    st.header("GreenHouse Gas Emmision")
+    st.text("이것은 건물부문 온실가스 계수 산정연구 용역입니다.")
 
 # 타이틀 적용 : 파일을 업로드해주세요.
 st.title(' 업로드해주세요.')
